6146163e90632ff58063eb59db626e4e67734eaa
Claas <claas.rostock@dnvgl.com>
2023-02-27 08:58:25 +0100

    added comments to demo_duck_typing_02

--- src/mypackage/demo_duck_typing_02.py
+++ src/mypackage/demo_duck_typing_02.py
@@ -3,8 +3,6 @@ from pathlib import Path
 
 from demo_duck_typing import print_name
 
-print_name(os)
-
-print_name(Path.cwd())
-
-print_name(1.23)
+print_name(os)  # ok
+print_name(Path.cwd())  # ok
+print_name(1.23)  # error
